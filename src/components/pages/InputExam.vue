b1783507f63d3e2da341d4817fe2255c036ba3f9
unknown <nguyenduyvan.55555@gmail.com>
2022-11-02 01:23:32 -0700

    initial

--- src/components/pages/InputExam.vue
+++ src/components/pages/InputExam.vue
@@ -1,27 +1,31 @@
 <template>
-    <div class="container">
-        <input class="input-search" placeholder="Nhập tên thành phố để tìm kiếm"/>
-    </div>
+  <div class="container">
+    <input class="input-search" placeholder="Nhập tên thành phố để tìm kiếm" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "InputExam",
-}
+  name: "InputExam",
+};
 </script>
 
 <style>
 .container {
-    width: 600px;
-    max-width: 600px;
-    margin: 20rem auto;
+  width: 600px;
+  max-width: 600px;
+  margin: 15rem auto;
 }
 .input-search {
-    width: 400px;
-    height: 49px;
-    border-radius: 4px;
-    padding: 8px, 10px, 8px, 10px;
-    color: rgba(229, 249, 255, 0.2);
+  padding: 8px, 10px, 8px, 10px !important;
+  width: 400px;
+  height: 49px;
+  border-radius: 4px;
+  color: #bfbfbf;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  border: 1px solid #dbdbdb;
+  background-color: #e5f9ff33;
 }
-
 </style>
